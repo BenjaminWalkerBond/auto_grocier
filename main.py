2e87de86a314b5cd50e208eed7d04271f00e463f
BenjaminWalkerBond <benbond96@gmail.com>
2023-09-12 02:16:18 -0500

    Pushing current work on reserve_time_slot

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def check_exists_by_xpath(xpath,driver):
 def random_time():
     time.sleep(random.randint(2, 3))
 def add_ingredient(ingredient, driver):
-    search_bar = driver.find_element(By.XPATH,"/html/body/div/header/div[1]/div[2]/div[1]/form/div/input")
+    search_bar = driver.find_element(By.ID, "search-input")
     search_bar.clear()
     random_time()
     if ingredient.get_tag() == "vegetable" or ingredient.get_tag() == "fruit":
@@ -34,7 +34,8 @@ def add_ingredient(ingredient, driver):
     search_bar.send_keys(Keys.ENTER)
     random_time()
     if(check_exists_by_xpath("/html/body/div[1]/main/div/div/div/div[2]/div[1]/div[2]/div/div[1]/div/div/div/div/div/div/button", driver)):
-        add_to_cart_button = driver.find_element(By.XPATH,"/html/body/div[1]/main/div/div/div/div[2]/div[1]/div[2]/div/div[1]/div/div/div/div/div/div/button")
+        add_to_cart_button = driver.find_element(By.XPATH,'//*[@id="search_product_grid"]/div[2]/div/div[1]/div/div/div/div/div/button')
+        # switch to relative xpath perhaps? //*[@id="search_product_grid"]/div[2]/div/div[1]/div/div/div/div/div/button
         add_to_cart_button.click()
     else:
         return 0
@@ -61,35 +62,71 @@ def reserve_time_slot(driver):
     
     # try:
         # navigate to the cart
-        cart_button = driver.find_element(By.XPATH, "/html/body/div/header/div[1]/div[2]/a[2]")
-        cart_button.click()
+        # cart_button = driver.find_element(By.XPATH, "/html/body/div/header/div[1]/div[2]/a[2]")
+        # cart_button = driver.find_element(By.CSS_SELECTOR, '[href="/cart/"]')
+        # cart_button.click()
+        driver.get("https://www.heb.com/cart/")
         random_time()
         # click the reserve time slot button
         reserve_button = driver.find_element(By.XPATH, "/html/body/div/main/div/div/div[1]/div/section[1]/div/div/button")
         reserve_button.click()
         random_time()
-        tomorrow = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div[2]/button[2]")
-        # find the evening slots sections by searching for all elements with the same class and an h3 tag with the text "Evening"
-        evening_slots_header = driver.find_element(By.XPATH, "//div[@class='sc-cyxg30-0 bQqawu']/div/h3[text()='Evening']")
-        # get the parent of the evening_slots element
+        # tomorrow = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div/div/div/div/div[2]/div[2]/button[2]")
+        # # find the evening slots sections by searching for all elements with the same class and an h3 tag with the text "Evening"
+        # evening_slots_header = driver.find_element(By.XPATH, "//div[@class='sc-cyxg30-0 bQqawu']/div/h3[text()='Evening']")
+
+        # Define the specific days you're looking for
+        days = ["Today", "Tomorrow","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+        # Iterate through the days list and search for matching elements
+        for day in days:
+            # Construct an XPath to find elements containing both the day and "Free"
+            xpath = f"//button[contains(descendant-or-self::text(), '{day}') and contains(descendant-or-self::text(), 'Free')]"
+            
+            # Find all matching elements with the specified XPath
+            matching_elements = driver.find_elements(By.XPATH, xpath)
+
+            if matching_elements:
+                # Click the first matching element found
+                print("found a free day!")
+                matching_elements[0].click()
+                time.sleep(3)
+                break  # Exit the loop once a match is found
+
+        # Define the specific word you're looking for
+        search_word = "Evening"
+
+        # Construct an XPath to find elements containing the search word
+        xpath = f"//*[contains(text(), '{search_word}')]"
+
+        # Find all matching elements on the page
+        evening_slots_header = driver.find_element(By.XPATH, xpath)
+        print(evening_slots_header)
+        # # get the parent of the evening_slots element
         evening_slots_container = (evening_slots_header.find_element(By.XPATH, "..")).find_element(By.XPATH, "..")
 
         evening_slots_container = evening_slots_container.find_elements(By.TAG_NAME, "button")
 
-        open_day =''
-        open_time = ''
         for time in evening_slots_container:
             # check if day.accesible_name contains open
             if "free" in time.get_attribute("aria-label"):
-                open_day = time
+                print("found a free time!")
+                time.click()
                 break
             print(time.accessible_name) 
 
 
+        # Define the specific word you're looking for
+        search_word = "Save"
+        # Construct an XPath to find elements containing the search word
+        xpath = f"//*[contains(text(), '{search_word}')]"
 
-        confirm_reserve = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div[2]/button[2]")
+        confirm_reserve = driver.find_element(By.XPATH, xpath)
         confirm_reserve.click()
         random_time()
+        close_modal_button = driver.find_element(By.CSS_SELECTOR, '[aria-label="Close Modal"]')
+        close_modal_button.click()
+        time.sleep(15)
     # except:
     #     print("Element not found")
     #     time.sleep(20)       
@@ -118,7 +155,6 @@ def login(driver):
         # login_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div/div[2]/form/fieldset/div[3]/button")
         login_button = driver.find_element(By.CSS_SELECTOR, '[data-cy="login-button"]')
         login_button.click()
-        time.sleep(60)
         random_time()
     else:
         print(f"{filename} not found in the current directory.")
@@ -159,7 +195,7 @@ if __name__ == '__main__':
 
     # perform HEB site operations
     login(driver)
-    time.sleep(10)
+    time.sleep(10) # necessary incase verification email sent
     random_time()
     driver.maximize_window()
     clear_cart(driver)
